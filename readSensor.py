92b163c1cf7cabd2ff75939a0af0f381ee8c347e
Chris Chan <chris.chan920204@gmail.com>
2023-12-20 04:18:17 +0900

    Add 2 byte conversion

--- readSensor.py
+++ readSensor.py
@@ -7,11 +7,17 @@ bus = smbus.SMBus(7)
 #Sensor Address 0x36
 address = 0x36
 
-def readValue():
-    value = bus.read_byte_data(address, 1)
+def readValue(n):
+    value = bus.read_byte_data(address, n)
     return value
 
 while True:
-    value = readValue()
-    print("Sensor",value)
+    byte1 = readValue(0)
+    byte2 = readValue(1)
+    byte3 = readValue(2)
+    byte4 = readValue(3)
+    current = (byte1 & 0x0F) << 8 + byte2
+    print("Current Value",current)
+    position = (byte3 & 0x0F) << 8 + byte4
+    print("Position Value", position)
     time.sleep(1)
